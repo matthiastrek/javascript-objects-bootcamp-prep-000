cf6225f7f8ae296457bac82b21b07d6e9022efcb
Matthew Trenk <matt.trenk@gmail.com>
2018-01-19 03:11:26 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {'beatles' : 'help'}
+function updatePlayList(playlist, artist, song){
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  
+}
