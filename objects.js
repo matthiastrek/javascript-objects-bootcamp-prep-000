3e39c79f2e869db6f293b34dea87a03538ccf73d
Matthew Trenk <matt.trenk@gmail.com>
2018-01-19 03:12:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'beatles' : 'help'}
-function updatePlayList(playlist, artist, song){
+function updatePlaylist(playlist, artist, song){
   playlist[artist] = song
   return playlist
 }
